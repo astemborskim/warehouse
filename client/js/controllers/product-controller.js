78d2f520daf58196430769c1c63994e04f198c92
astemborskim <astemborskim@gmail.com>
2015-03-22 17:24:14 -0500

    Filter Search Working

--- client/js/controllers/product-controller.js
+++ client/js/controllers/product-controller.js
@@ -3,8 +3,25 @@ app.controller('productController', ['$scope', '$resource',
 
 	var Inventory = $resource('/api/inventory', {});
 
-	Inventory.query(function (results){
-		$scope.inventory=results;
-		console.log(JSON.stringify(results))
-	})
+	getInventory = function(req, res){
+		Inventory.query(function (results){
+			$scope.inventory=results;
+			console.log(JSON.stringify(results));
+			//console.log(inventory);
+	})}
+
+	$scope.orderBySKU = 'SKU';
+	$scope.orderByName = 'Product_Name';
+	$scope.viewLimit = 10;
+
+	getInventory();
+
+	$scope.loadSKUSearch = function(){
+			$scope.search.Product_Name="";
+			$scope.search.SKU = "";
+	}
+	$scope.loadNameSearch = function(){
+			$scope.search.SKU = "";
+			$scope.search.Product_Name="";	
+	}
 }]);
