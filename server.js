9e5a6b1c21d74e2afbea715e4db089f0fcfb1923
unknown <Matthew.Astemborski@ASTEMBOR-M-W72.network.internal>
2015-04-06 11:40:04 -0500

    dynamic port for heroku

--- server.js
+++ server.js
@@ -122,6 +122,6 @@ app.get('/redirectHome', serverController.getHome);
 
 
 //Start server on port 3000
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
 	console.log("Server is listening...");
 });
