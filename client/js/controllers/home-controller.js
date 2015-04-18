cb459f0830c8a9f18b3017a8f23d06f3f8d660c7
Matthew <astemborskim@gmail.com>
2015-04-18 02:45:09 -0500

    Home SKU Scan

--- client/js/controllers/home-controller.js
+++ client/js/controllers/home-controller.js
@@ -1,7 +1,8 @@
 app.controller('homeController', ['$scope', '$resource', function ($scope, $resource) {
 
 	$scope.oneItem ={};
-	$scope.showskuMessage = false;
+	$scope.showResults = false;
+	$scope.noResults = false;
 
 	var Item = $resource('/api/item/:id', {'id' : '@SKU'});//, {update : {method : 'PUT'}});
 
@@ -10,20 +11,23 @@ app.controller('homeController', ['$scope', '$resource', function ($scope, $reso
 		var SKU = SKU.toString();
 		var item = new Item({'SKU' : SKU});
 		item.$get({ id : SKU }, function (results){
-			console.log(results);
+			//console.log(results);
 			if(results.Product_Name == null){
+				$scope.noResults = true;
 				console.log('Item with specified SKU does not exist');
 			}
-			// else{
-			// 	console.log('RESULTS: ' + JSON.stringify(results));
-			// 	$scope.oneItem.SKU = results.SKU;
-			// 	$scope.oneItem.Product_Name = results.Product_Name;
-			// 	$scope.oneItem.Product_Description = results.Product_Description;
-			// 	$scope.oneItem.Product_Location = results.Product_Location;
-			// 	$scope.oneItem.Quantity = results.Quantity;
+			else{
+				$scope.showResults = true;
+				$scope.noResults = false;
+				console.log('Product Found');
+				$scope.oneItem.SKU = results.SKU;
+				$scope.oneItem.Product_Name = results.Product_Name;
+				$scope.oneItem.Product_Description = results.Product_Description;
+				$scope.oneItem.Product_Location = results.Product_Location;
+				$scope.oneItem.Quantity = results.Quantity;
 				
-			// 	console.log($scope.oneItem);	
-			// }
+				console.log($scope.oneItem);	
+			}
 			
 		})
 	}
