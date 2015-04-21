23159a1df1c63789d73bd832d0ab4aa7cedd6e92
unknown <Matthew.Astemborski@ASTEMBOR-M-W72.network.internal>
2015-04-21 16:59:18 -0500

    DB path changed - Joynet to Google service host

--- server/db/db.js
+++ server/db/db.js
@@ -1,4 +1,5 @@
 module.exports = {
-	'url' : 'mongodb://test:test@ds045998.mongolab.com:45998/warehouse' //mongolab
+	//'url' : 'mongodb://test:test@ds045998.mongolab.com:45998/warehouse' //mongolab on Joyent - discontinued
 	//'url' : 'mongodb://localhost/warehouse' //local
+	'url' : 'mongodb://test:test@ds055689.mongolab.com:55689/acmewarehouse' //mongolab google sandbox
 }
